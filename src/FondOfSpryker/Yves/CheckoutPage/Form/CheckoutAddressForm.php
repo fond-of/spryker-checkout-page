a0b21a0f3d21343c34fcdd43cb0e92a8533695f6
Pascal Fischer <pascal.fischer@fondof.de>
2018-11-14 14:00:45 +0100

    Add regex pattern for email validation

--- src/FondOfSpryker/Yves/CheckoutPage/Form/CheckoutAddressForm.php
+++ src/FondOfSpryker/Yves/CheckoutPage/Form/CheckoutAddressForm.php
@@ -5,11 +5,18 @@ namespace FondOfSpryker\Yves\CheckoutPage\Form;
 use SprykerShop\Yves\CustomerPage\Form\CheckoutAddressForm as SprykerShopCheckoutAddressForm;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class CheckoutAddressForm extends SprykerShopCheckoutAddressForm
 {
     const FIELD_EMAIL = 'email';
+
+    const VALIDATE_REGEX_EMAIL = "/^[A-ZÄÖÜa-zäöü0-9._%+\&\-ß!]+@[a-zäöüA-ZÄÖÜ0-9.\-ß]+\.[a-zäöüA-ZÄÖÜ]{2,}$/ix";
+
+    const VALIDATE_MIN_LENGTH = 3;
 
     /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
@@ -39,16 +46,53 @@ class CheckoutAddressForm extends SprykerShopCheckoutAddressForm
             'required' => true,
             'constraints' => [
                 $this->createEmailValidConstraints($options),
+                $this->createRegexEmailConstraint($options),
+                $this->createMinLengthConstraint($options),
             ],
         ]);
 
         return $this;
     }
 
-    protected function createEmailValidConstraints(array $options)
+    /**
+     * @param array $options
+     * @return \Symfony\Component\Validator\Constraint
+     */
+    protected function createEmailValidConstraints(array $options): Constraint
     {
         return new Email([
             'message' => 'email.validation.error',
+        ]);
+    }
+
+    /**
+     * @param array $options
+     * @return \Symfony\Component\Validator\Constraint
+     */
+    protected function createRegexEmailConstraint(array $options): Constraint
+    {
+        $validationGroup = $this->getValidationGroup($options);
+
+        return new Regex([
+            'pattern' => static::VALIDATE_REGEX_EMAIL,
+            'message' => 'validation.regex.email.message',
+            'groups' => $validationGroup,
+        ]);
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return \Symfony\Component\Validator\Constraints\Length
+     */
+    protected function createMinLengthConstraint(array $options)
+    {
+        $validationGroup = $this->getValidationGroup($options);
+
+        return new Length([
+            'min' => static::VALIDATE_MIN_LENGTH,
+            'groups' => $validationGroup,
+            'minMessage' => 'validation.length.min.message',
         ]);
     }
 }
