89be51371481096901360d0af2a9254d6985940f
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-06-02 20:38:58 +0200

    - added support for new symfony router

---
+++ src/FondOfSpryker/Yves/CheckoutPage/Plugin/Router/CheckoutPageRouteProviderPlugin.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace FondOfSpryker\Yves\CheckoutPage\Plugin\Router;
+
+use FondOfSpryker\Shared\CheckoutPage\CheckoutPageConstants;
+use Spryker\Yves\Router\Route\RouteCollection;
+use SprykerShop\Yves\CheckoutPage\Plugin\Router\CheckoutPageRouteProviderPlugin as SprykerCheckoutPageRouteProviderPlugin;
+
+class CheckoutPageRouteProviderPlugin extends SprykerCheckoutPageRouteProviderPlugin
+{
+    /**
+     * @param \Spryker\Yves\Router\Route\RouteCollection $routeCollection
+     *
+     * @return \Spryker\Yves\Router\Route\RouteCollection
+     */
+    public function addRoutes(RouteCollection $routeCollection): RouteCollection
+    {
+        $routeCollection = parent::addRoutes($routeCollection);
+        $routeCollection = $this->addBillingAddressStepRoute($routeCollection);
+        $routeCollection = $this->addShippingAddressStepRoute($routeCollection);
+        $routeCollection = $this->addRegionByCountry($routeCollection);
+
+        return $routeCollection;
+    }
+
+    /**
+     * @param \Spryker\Yves\Router\Route\RouteCollection $routeCollection
+     *
+     * @return \Spryker\Yves\Router\Route\RouteCollection
+     */
+    protected function addBillingAddressStepRoute(RouteCollection $routeCollection): RouteCollection
+    {
+        $route = $this->buildRoute('/checkout/billing-address', 'CheckoutPage', 'Checkout', 'billingAddress');
+        $route = $route->setMethods(['GET', 'POST']);
+        $routeCollection->add(CheckoutPageConstants::ROUTE_CHECKOUT_BILLING_ADDRESS, $route);
+
+        return $routeCollection;
+    }
+
+    /**
+     * @param \Spryker\Yves\Router\Route\RouteCollection $routeCollection
+     *
+     * @return $this|\Spryker\Yves\Router\Route\RouteCollection
+     */
+    protected function addShippingAddressStepRoute(RouteCollection $routeCollection): RouteCollection
+    {
+        $route = $this->buildRoute('/checkout/shipping-address', 'CheckoutPage', 'Checkout', 'shippingAddress');
+        $route = $route->setMethods(['GET', 'POST']);
+        $routeCollection->add(CheckoutPageConstants::ROUTE_CHECKOUT_SHIPPING_ADDRESS, $route);
+
+        return $routeCollection;
+    }
+
+    /**
+     * @param \Spryker\Yves\Router\Route\RouteCollection $routeCollection
+     *
+     * @return $this|\Spryker\Yves\Router\Route\RouteCollection
+     */
+    protected function addRegionByCountry(RouteCollection $routeCollection): RouteCollection
+    {
+        $route = $this->buildRoute(
+            '/checkout/region-by-country/{country}',
+            'CheckoutPage',
+            'Checkout',
+            'regionsByCountry'
+        );
+        $route
+            ->setMethods(['GET'])
+            ->setRequirement('country', 'de');
+        $routeCollection->add(CheckoutPageConstants::ROUTE_CHECKOUT_REGION_BY_COUNTRY, $route);
+
+        return $routeCollection;
+    }
+}
