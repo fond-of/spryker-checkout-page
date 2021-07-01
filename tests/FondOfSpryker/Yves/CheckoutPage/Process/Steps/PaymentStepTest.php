8d3312ff16f756b60f8251014281385785f46270
Julian Hyatt <julian.hyatt@fondof.de>
2021-07-01 11:03:33 +0200

    fix tests

--- tests/FondOfSpryker/Yves/CheckoutPage/Process/Steps/PaymentStepTest.php
+++ tests/FondOfSpryker/Yves/CheckoutPage/Process/Steps/PaymentStepTest.php
@@ -4,10 +4,17 @@ namespace FondOfSpryker\Yves\CheckoutPage\Process\Steps;
 
 use Codeception\Test\Unit;
 use Generated\Shared\Transfer\QuoteTransfer;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+use Spryker\Shared\Log\Config\LoggerConfigInterface;
+use Spryker\Shared\Log\LoggerFactory;
 use Spryker\Yves\Messenger\FlashMessenger\FlashMessenger;
+use Spryker\Yves\Messenger\FlashMessenger\FlashMessengerInterface;
 use Spryker\Yves\StepEngine\Dependency\Plugin\Handler\StepHandlerPluginCollection;
 use SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToCalculationClientBridge;
+use SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToCalculationClientInterface;
 use SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToPaymentClientBridge;
+use SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToPaymentClientInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class PaymentStepTest extends Unit
@@ -23,6 +30,11 @@ class PaymentStepTest extends Unit
     private $requestMock;
 
     /**
+     * @var \Psr\Log\LoggerInterface|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $loggerMock;
+
+    /**
      * @var \FondOfSpryker\Yves\CheckoutPage\Process\Steps\PaymentStep
      */
     private $paymentStep;
@@ -33,6 +45,7 @@ class PaymentStepTest extends Unit
                                     ->onlyMethods(['setCheckoutConfirmed', 'setOrderReference', 'setIdSalesOrder', 'getCheckoutConfirmed'])
                                     ->getMock();
         $this->requestMock = $this->getMockBuilder(Request::class)->getMock();
+        $this->loggerMock = $this->getMockBuilder(Logger::class)->disableOriginalConstructor()->getMock();
         $paymentClientMock = $this->getMockBuilder(CheckoutPageToPaymentClientBridge::class)
                                     ->disableOriginalConstructor()
                                     ->getMock();
@@ -44,15 +57,49 @@ class PaymentStepTest extends Unit
                                     ->disableOriginalConstructor()
                                     ->getMock();
 
-        $this->paymentStep = new PaymentStep(
+        $this->paymentStep = new class(
             $paymentClientMock,
             $paymentMethodHandlerMocker,
             '',
             '',
             $flashMessagenerMock,
-            $calculationClientMock,
-            []
-        );
+            $calculationClientMock
+            [],
+            $this->loggerMock) extends PaymentStep {
+
+            /**
+             * @var \Psr\Log\LoggerInterface
+             */
+            protected $loggerMock;
+
+            /**
+             *  constructor.
+             *
+             * @param \SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToPaymentClientInterface $paymentClient
+             * @param \Spryker\Yves\StepEngine\Dependency\Plugin\Handler\StepHandlerPluginCollection $paymentPlugins
+             * @param $stepRoute
+             * @param $escapeRoute
+             * @param \Spryker\Yves\Messenger\FlashMessenger\FlashMessengerInterface $flashMessenger
+             * @param \SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToCalculationClientInterface $calculationClient
+             * @param array $checkoutPaymentStepEnterPreCheckPlugins
+             * @param \Psr\Log\LoggerInterface $loggerMock
+             */
+            public function __construct(CheckoutPageToPaymentClientInterface $paymentClient, StepHandlerPluginCollection $paymentPlugins, $stepRoute, $escapeRoute, FlashMessengerInterface $flashMessenger, CheckoutPageToCalculationClientInterface $calculationClient, array $checkoutPaymentStepEnterPreCheckPlugins, LoggerInterface $loggerMock)
+            {
+                parent::__construct($paymentClient, $paymentPlugins, $stepRoute, $escapeRoute, $flashMessenger, $calculationClient, $checkoutPaymentStepEnterPreCheckPlugins);
+                $this->loggerMock = $loggerMock;
+            }
+
+            /**
+             * @param \Spryker\Shared\Log\Config\LoggerConfigInterface|null $loggerConfig
+             *
+             * @return \Psr\Log\LoggerInterface|null
+             */
+            protected function getLogger(?LoggerConfigInterface $loggerConfig = null)
+            {
+                return $this->loggerMock;
+            }
+        };
     }
 
     /**
