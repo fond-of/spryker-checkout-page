584bba4f2642af4cf96f69ae7969935352dc883d
Pascal Fischert <p-fischer@gmx.ne>
2022-07-18 14:33:26 +0200

    filter shipping-address countries by blacklisted-countries

--- src/FondOfSpryker/Yves/CheckoutPage/Form/DataProvider/CheckoutShippingAddressFormDataProvider.php
+++ src/FondOfSpryker/Yves/CheckoutPage/Form/DataProvider/CheckoutShippingAddressFormDataProvider.php
@@ -2,17 +2,6 @@
 
 namespace FondOfSpryker\Yves\CheckoutPage\Form\DataProvider;
 
-use Generated\Shared\Transfer\QuoteTransfer;
-
 class CheckoutShippingAddressFormDataProvider extends CheckoutBillingAddressFormDataProvider
 {
-    /**
-     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
-     *
-     * @return array
-     */
-    protected function getAvailableCountries(QuoteTransfer $quoteTransfer): array
-    {
-        return $this->countryDataProvider->getCountries($quoteTransfer);
-    }
 }
