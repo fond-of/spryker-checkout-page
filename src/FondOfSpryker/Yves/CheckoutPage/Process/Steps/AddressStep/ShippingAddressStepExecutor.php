40c6ca0e212e0fd32f7dfba784510c9158dce548
SpiderDan98 <40319023+SpiderDan98@users.noreply.github.com>
2022-08-25 11:26:13 +0200

    ✨ Feature: add order reference resetter (#22)

--- src/FondOfSpryker/Yves/CheckoutPage/Process/Steps/AddressStep/ShippingAddressStepExecutor.php
+++ src/FondOfSpryker/Yves/CheckoutPage/Process/Steps/AddressStep/ShippingAddressStepExecutor.php
@@ -2,15 +2,40 @@
 
 namespace FondOfSpryker\Yves\CheckoutPage\Process\Steps\AddressStep;
 
+use FondOfSpryker\Yves\CheckoutPage\Resetter\OrderReferenceResetterInterface;
 use Generated\Shared\Transfer\AddressTransfer;
 use Generated\Shared\Transfer\CustomerTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
 use Generated\Shared\Transfer\ShipmentTransfer;
+use SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToCustomerClientInterface;
+use SprykerShop\Yves\CheckoutPage\Dependency\Service\CheckoutPageToCustomerServiceInterface;
 use SprykerShop\Yves\CheckoutPage\Process\Steps\AddressStep\AddressStepExecutor as SprykerAddressStepExecutor;
 use Symfony\Component\HttpFoundation\Request;
 
 class ShippingAddressStepExecutor extends SprykerAddressStepExecutor
 {
+    /**
+     * @var \FondOfSpryker\Yves\CheckoutPage\Resetter\OrderReferenceResetterInterface
+     */
+    protected $orderReferenceResetter;
+
+    /**
+     * @param \SprykerShop\Yves\CheckoutPage\Dependency\Service\CheckoutPageToCustomerServiceInterface $customerService
+     * @param \SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToCustomerClientInterface $customerClient
+     * @param array $addressTransferExpanderPlugins
+     * @param \FondOfSpryker\Yves\CheckoutPage\Resetter\OrderReferenceResetterInterface $orderReferenceResetter
+     */
+    public function __construct(
+        CheckoutPageToCustomerServiceInterface $customerService,
+        CheckoutPageToCustomerClientInterface $customerClient,
+        array $addressTransferExpanderPlugins,
+        OrderReferenceResetterInterface $orderReferenceResetter
+    ) {
+        parent::__construct($customerService, $customerClient, $addressTransferExpanderPlugins);
+
+        $this->orderReferenceResetter = $orderReferenceResetter;
+    }
+
     /**
      * @param \Symfony\Component\HttpFoundation\Request $request
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
@@ -24,6 +49,7 @@ class ShippingAddressStepExecutor extends SprykerAddressStepExecutor
         $quoteTransfer = $this->updateQuoteShipmentTransfer($quoteTransfer);
         $quoteTransfer = $this->hydrateShippingAddress($quoteTransfer, $customerTransfer);
         $quoteTransfer = $this->setQuoteShippingAddress($quoteTransfer, $customerTransfer);
+        $quoteTransfer = $this->orderReferenceResetter->reset($quoteTransfer);
 
         return $quoteTransfer;
     }
---
+++ tests/FondOfSpryker/Yves/CheckoutPage/Resetter/OrderReferenceResetterTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FondOfSpryker\Yves\CheckoutPage\Resetter;
+
+class OrderReferenceResetterTest
+{
+
+}
